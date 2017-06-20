70648d9d6f0657e014f77696f0d5ed0bc709479d
Kristina Kovalevskaya <kristina.kovalevskaya@oxagile.com>
2017-06-20 19:15:59 +0300

    Working row

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/KristinaEtc/config"
+
 	"github.com/ventu-io/slf"
 )
 
@@ -13,7 +14,9 @@ var log = slf.WithContext("WS-test")
 
 // ConfFile is a file with all program options
 type ConfFile struct {
-	Name string
+	Name              string
+	Address           string
+	MessageSendPeriod int
 	/*WriteWait      time.Duration
 	PongWait       time.Duration
 	PingPeriod     time.Duration
@@ -21,22 +24,37 @@ type ConfFile struct {
 }
 
 var globalOpt = ConfFile{
-	Name: "WS-test",
+	Name:              "WS-test",
+	Address:           "localhost:7777",
+	MessageSendPeriod: 5,
 	/*	WriteWait:      10 * time.Second,
 		PongWait:       10 * time.Second,
 		PingPeriod:     5 * time.Second,
 		MaxMessageSize: 1024 * 1024,*/
 }
 
+var data []byte
+
 func main() {
 
 	config.ReadGlobalConfig(&globalOpt, "WS-options")
 	log.Infof("%+v", globalOpt)
 
+	var err error
+	//	data, err = ioutil.ReadFile("test-data2")
+	//	if err != nil {
+	//		log.Errorf("No file with test data. Exiting")
+	//		os.Exit(1)
+
+	//	} else {
+	//		log.Debug("all ok")
+	//	}
+
 	go h.run()
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	http.HandleFunc("/ws", serveWs)
-	err := http.ListenAndServe(":8080", nil)
+	log.Debug(globalOpt.Address)
+	err = http.ListenAndServe(globalOpt.Address, nil)
 	if err != nil {
 		log.Errorf("Listen&Serve: %s", err.Error())
 	}
